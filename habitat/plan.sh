cb706f3a004501e7f9313e0f31e76cc8937e82ca
jahidul-arafat <jahidapon@gmail.com>
2021-09-07 14:58:46 +0600

    updated

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,7 +1,7 @@
 pkg_name=meme-machine
-pkg_origin=learnchef_tryhabitat
-pkg_version="0.2.0"
+pkg_origin=lab_hab_originkey
+pkg_version="0.1.0"
 pkg_scaffolding="core/scaffolding-ruby"
 pkg_deps=(
-  learnchef_tryhabitat/imagemagick
+  core/imagemagick
 )
